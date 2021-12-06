30c7a9b9176f1e77e6fb39321bdbfb28f8fa8d02
Dr.Drunkestien <pratham20453@iiitd.ac.in>
2021-12-07 00:08:56 +0530

    Home Screen with clouds

--- src/main/java/com/willhero/willhero/HelloApplication.java
+++ src/main/java/com/willhero/willhero/HelloApplication.java
@@ -3,6 +3,7 @@ package com.willhero.willhero;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
 
     public void start(Stage stage) throws IOException {
-
+        //ImageView
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomeScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 960, 540);
         stage.setTitle("Hello!");
